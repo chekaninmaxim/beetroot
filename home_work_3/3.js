104904861d490c55afddef39628b9ee13f839808
chekaninmaxim <chekaninm@gmail.com>
2020-10-26 21:54:24 +0200

    fix task 3

--- home_work_3/3.js
+++ home_work_3/3.js
@@ -2,26 +2,24 @@
 Код работает, но не защищен - клментский код может удалить элементы массива  из-вне
 Исправить этот недостаток 
 */
-(function() {
 
-  function getUsers() {
-    const users = ["Bill"];
-    return {
-      addUser: function(name) {
-        users.push(name);
-        return users;
-      },
-      getUsers: function() {
-        return users;
-      }
-    };
-  }
+function getUsers() {
+  const users = ["Bill"];
+  return {
+    addUser: function(name) {
+      users.push(name);
+      return users.slice();
+    },
+    getUsers: function() {
+      return users.slice();
+    }
+  };
+}
 
-  const u = getUsers();
-  u.addUser("Jim");
-  u.addUser("Paul");
-  console.log(u.getUsers()); // выведется [ 'Bill', 'Jim', 'Paul' ]
+const u = getUsers();
+u.addUser("Jim");
+u.addUser("Paul");
+console.log(u.getUsers()); // выведется [ 'Bill', 'Jim', 'Paul' ]
 
-  u.getUsers().pop(); // удаляем 2-й элемент массива
-  console.log(u.getUsers()); //  ['Bill', 'Jim']
-})()
+u.getUsers().pop(); // удаляем 2-й элемент массива
+console.log(u.getUsers()); //  ['Bill', 'Jim']
