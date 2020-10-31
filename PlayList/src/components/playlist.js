6aaa4fb8440cc11c13bb244a3b494e7c2b497762
chekanin <chekaninm@gmail.com>
2020-10-31 20:25:39 +0200

    Implemented first task

--- PlayList/src/components/playlist.js
+++ PlayList/src/components/playlist.js
@@ -1,36 +1,39 @@
-function PlayList() {
-	this.songs = [];
-	this.currentIndex = 0;
-}
-
-PlayList.prototype.add = function(song) {
-	this.songs.push(song);
-}
-
-PlayList.prototype.play = function() {
-	const currentSong = this.songs[this.currentIndex];
-	currentSong.play();
-}
-
-PlayList.prototype.stop = function() {
-	const currentSong = this.songs[this.currentIndex];
-	currentSong.stop();
-}
-
-PlayList.prototype.next = function() {
-	this.stop();
-	this.currentIndex++;
-	if (this.currentIndex === this.songs.length) {
+class PlayList {
+	constructor() {
+		this.songs = [];
 		this.currentIndex = 0;
-	} 
-	this.play();
+	}
+
+	add(song) {
+		this.songs.push(song);
+	}
+
+	play() {
+		const currentSong = this.songs[this.currentIndex];
+		currentSong.play();
+	}
+
+	stop() {
+		const currentSong = this.songs[this.currentIndex];
+		currentSong.stop();
+	}
+
+	next() {
+		this.stop();
+		this.currentIndex++;
+		if (this.currentIndex === this.songs.length) {
+			this.currentIndex = 0;
+		}
+		this.play();
+	}
+
+	render(list) {
+		list.innerHTML = "";
+		return this.songs.forEach(song => {
+			list.innerHTML += song.toHtml();
+		});
+	}
 }
 
-PlayList.prototype.render = function(list) {
-	list.innerHTML = "";
-	return this.songs.forEach(song => {
-		list.innerHTML += song.toHtml();
-	});
-}
 
 export default PlayList;
